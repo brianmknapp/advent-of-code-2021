d048f4660cc1f33c5d300339b0cfab0830946ff5
Brian Knapp <bknapp3@paychex.com>
2021-12-01 08:02:10 -0500

    Days 2 complete

--- aoc2021/day1.py
+++ aoc2021/day1.py
@@ -1,6 +1,19 @@
 from typing import List
 
 
+def calculate_three_measurement_window_increases(depths: List[int]) -> int:
+    sums: List[int] = []
+    n_minus_1: int = None
+    n_minus_2: int = None
+    for depth in depths:
+        if n_minus_1 is not None and n_minus_2 is not None:
+            sums.append(depth + n_minus_1 + n_minus_2)
+        n_minus_2 = n_minus_1
+        n_minus_1 = depth
+
+    return calculate_depth_measurement_increases(sums)
+
+
 def calculate_depth_measurement_increases(depths: List[int]) -> int:
     previous: int = None
     increases: int = 0
@@ -20,6 +33,9 @@ def main(filename: str):
 
     larger_measurements = calculate_depth_measurement_increases(lines)
     print(f'Larger Measurements: {larger_measurements}')
+
+    larger_three_window_measurements = calculate_three_measurement_window_increases(lines)
+    print(f'Larger Three-Window Measurements: {larger_three_window_measurements}')
 
 
 if __name__ == '__main__':
