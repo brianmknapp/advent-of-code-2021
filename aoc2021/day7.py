f77cc32e4091884f5b314d4a9acd256c2f1ff01f
Brian Knapp <brianmknapp@gmail.com>
2021-12-07 07:47:13 -0500

    Part 1 complete

---
+++ aoc2021/day7.py
@@ -0,0 +1,32 @@
+from collections import Counter
+from pathlib import Path
+from typing import List, Dict
+
+
+class CrabCollection:
+    def __init__(self, initial_lineup: List[int]):
+        self.lineup = initial_lineup
+
+    def calculate_most_efficient_adjustment(self) -> Dict[int, List[int]]:
+        result: Dict[int, List[int]] = {}
+        for v in range(max(self.lineup)):
+            result[v] = [abs(v - x) for x in self.lineup]
+        return result
+
+
+def main(file_path: Path):
+    with open(file_path) as file:
+        line = file.readline()
+    input_data = [int(x) for x in line.split(',')]
+    crabs = CrabCollection(input_data)
+    efficient_adjustments = crabs.calculate_most_efficient_adjustment()
+    most_efficient = min([sum(v) for k, v in efficient_adjustments.items()])
+    print(f'Most Efficient Adjustment: {most_efficient}')
+
+
+if __name__ == '__main__':
+    p = Path('../input/day7_test.txt')
+    main(p)
+
+    p = Path('../input/day7_1.txt')
+    main(p)
