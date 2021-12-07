c9831b86a2ab92922ff2cc484db9560dfefda7d7
Brian Knapp <brianmknapp@gmail.com>
2021-12-07 08:00:39 -0500

    Part 2 complete
    Thanks, I hate it

--- aoc2021/day7.py
+++ aoc2021/day7.py
@@ -3,6 +3,13 @@ from pathlib import Path
 from typing import List, Dict
 
 
+def calculate_triangular_value(count: int) -> int:
+    result: int = 0
+    for i in range(count):
+        result += i
+    return result
+
+
 class CrabCollection:
     def __init__(self, initial_lineup: List[int]):
         self.lineup = initial_lineup
@@ -10,7 +17,7 @@ class CrabCollection:
     def calculate_most_efficient_adjustment(self) -> Dict[int, List[int]]:
         result: Dict[int, List[int]] = {}
         for v in range(max(self.lineup)):
-            result[v] = [abs(v - x) for x in self.lineup]
+            result[v] = [abs(v - x) + calculate_triangular_value(abs(v - x)) for x in self.lineup]
         return result
 
 
